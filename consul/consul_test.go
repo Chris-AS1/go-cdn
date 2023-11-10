d6f02baba61127697295c78ca470177a203042f1
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-11-10 18:46:14 +0100

    refactored and fixed consul, tracing tests

--- consul/consul_test.go
+++ consul/consul_test.go
@@ -7,62 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var cfg *config.Config
+func TestConsul(t *testing.T) {
+	var consul_client *ConsulClient
 
-func loadConfigsAndGetClient(t *testing.T) (*config.Config, *ConsulClient) {
-	c, err := config.NewConfig()
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-    
-    // Makes it so there's only 1 instance of it, so that Consul service_id doesn't get regenerated
-    if cfg == nil {
-        cfg = &c
-    }
-
-	consul_client, err := NewConsulClient(cfg)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	return cfg, consul_client
-}
-
-func TestConsulGetClient(t *testing.T) {
 	cfg, err := config.NewConfig()
+	assert.Nil(t, err)
+
+	t.Run("TestConsulConnection", func(t *testing.T) {
+		consul_client, err = NewConsulClient(&cfg)
+		assert.Nil(t, err)
+	})
+
+	// Don't even attempt to run other tests if a client isn't returned
 	if err != nil {
-		assert.FailNow(t, err.Error())
+		return
 	}
 
-	_, err = NewConsulClient(&cfg)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
+	t.Run("TestConsulRegistration", func(t *testing.T) {
+		err := consul_client.RegisterService(&cfg)
+		assert.Nil(t, err)
+	})
+
+	t.Run("TestConsulDeregistration", func(t *testing.T) {
+		err := consul_client.DeregisterService(&cfg)
+		assert.Nil(t, err)
+	})
+
+	t.Run("TestConsulServiceDiscovery", func(t *testing.T) {
+		address, port, err := consul_client.DiscoverService(cfg.DatabaseProvider.DatabaseAddress)
+		assert.Nil(t, err)
+		assert.NotSame(t, address, "")
+		assert.NotSame(t, port, 0)
+	})
 }
-
-func TestConsulRegistration(t *testing.T) {
-	_, consul_client := loadConfigsAndGetClient(t)
-	err := consul_client.RegisterService(cfg)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-}
-
-func TestConsulDeregistration(t *testing.T) {
-	_, consul_client := loadConfigsAndGetClient(t)
-	err := consul_client.DeregisterService(cfg)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-}
-
-func TestConsulServiceDiscovery(t *testing.T) {
-	_, consul_client := loadConfigsAndGetClient(t)
-	// Discovers postgres from Consul
-	address, port, err := consul_client.DiscoverService(cfg.DatabaseProvider.DatabaseAddress)
-	if err != nil {
-		assert.FailNow(t, err.Error())
-	}
-	assert.NotSame(t, address, "")
-	assert.NotSame(t, port, 0)
-}
-
